88fa2151d71925fabcdc7f5fffd713fbc5283466
sosuisen <hidekazu.kubota@gmail.com>
2023-03-23 19:37:42 +0900

    doc: comments

--- src/main/java/com/example/MyApp.java
+++ src/main/java/com/example/MyApp.java
@@ -20,36 +20,34 @@ public class MyApp extends Application {
 		// イベントハンドラを追加することで、変更があったときに処理を行うことができます。
 		ObservableList<String> model = listView.getItems();
 
-		// model変更時に呼ばれるイベントハンドラの追加。
-		// 単にラムダ式 c -> {} と書ければ良いのですが、
-		// ObserbavleListのイベントハンドラには
-		// ListChangeListenerとInvalidationListenerの２通りがあるため、
-		// ListChangeListenerのほうであることがコンパイラに判るよう型を書く必要があります。
+		/** 
+		 * model変更時に呼ばれるイベントハンドラの追加。
+		 *   単にラムダ式 c -> {} と書ければ良いのですが、
+		 *   ObserbavleListのイベントハンドラには
+		 *   ListChangeListenerとInvalidationListenerの２通りがあるため、
+		 *   ListChangeListenerのほうであることがコンパイラに判るよう型を書く必要があります。
+		 */
 		model.addListener((ListChangeListener.Change<? extends String> c) -> {
-			// 変更箇所のみファイルへ保存する、といった処理ができます。
-			// 今回はコンソールへ出力するにとどめます。
+			// 変更箇所のみファイルへ保存する、といった処理ができます。今回はコンソールへ出力するにとどめます。
 			while (c.next()) {
 				// cにはモデルの変更内容がリストとして入っているため、whileで一つずつ取り出します。
 				if (c.wasReplaced()) {
-					// getAddedSublist()で今回追加があった内容の取り出し。
-					// リストの項目を書き換えた場合は、旧内容の削除、新内容の追加が行われるので、
-					// 追加分をgetAddedSublist()で取り出すことができます。
+					/**
+					 *  getAddedSublist()で今回追加があった内容の取り出し。
+					 *  リストの項目を書き換えた場合は、旧内容の削除、新内容の追加が行われるので、
+					 *  追加分をgetAddedSublist()で取り出すことができます。
+					 */
 					c.getAddedSubList().forEach(str -> System.out.println("書き換え[" + c.getFrom() + "] " + str));
 				} else if (c.wasAdded()) {
 					c.getAddedSubList().forEach(str -> System.out.println("追加[" + c.getFrom() + "]" + str));
 				}
 			}
-			/* 変更のたびにモデルの全内容を出力するならこんな感じ。 
-			model.forEach(str -> {
-				System.out.println(str);
-			});
-			*/
 		});
 
+		// セルファクトリを指定
+		listView.setCellFactory(TextFieldListCell.forListView());
 		// 編集できるようにする
 		listView.setEditable(true);
-		// セルファクトリクラスを指定
-		listView.setCellFactory(TextFieldListCell.forListView());
 		// 編集完了時に呼ばれるイベントハンドラを追加
 		listView.setOnEditCommit(e -> {
 			// 編集されたセルの値を取得
